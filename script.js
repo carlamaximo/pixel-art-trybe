8130d9be21a3cd6a57a4e571bd4f2a50f416517e
Carla <carladmaximo@gmail.com>
2021-07-08 16:25:40 -0300

    Mais alguns requisitos obrigatórios

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var mouseSelected = document.getElementsByClassName('selected')
+const colorBlack = document.getElementsByClassName('color')[0];
+const colorOne = document.getElementsByClassName('color')[1];
+const colorTwo = document.getElementsByClassName('color')[2];
+const colorThree = document.getElementsByClassName('color')[3];
+const initialSize = 5;
 
-mouseSelected.target.style.color = 'black';
+colorBlack.classList.add('selected');
+
+colorBlack.addEventListener('click', selectColor);
+colorOne.addEventListener('click', selectColor);
+colorTwo.addEventListener('click', selectColor);
+colorThree.addEventListener('click', selectColor);
+
+
