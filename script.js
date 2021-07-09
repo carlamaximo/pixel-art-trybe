9ec204a551940b5ca37697e70b5dce6dc8a9f7c4
Carla <carladmaximo@gmail.com>
2021-07-09 11:10:35 -0300

    Requiriments 4, 5

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ function pixelGrid() {
   const pixelGrid = document.getElementById('pixel-board');
 
   for (let rows = 0; rows < 5; rows += 1) {
-    const row = document.createElement('div');
+    const row = document.createElement('section');
 
     for (let columns = 0; columns < 5; columns += 1) {
-      const pixel = document.createElement('div')
+      const pixels = document.createElement('div')
 
-      pixel.classList.add('pixel');
-      row.appendChild(pixel);
+      pixels.classList.add('pixel');
+      row.appendChild(pixels);
       
     }
     pixelGrid.appendChild(row);
+    
   }
 }
 pixelGrid()
